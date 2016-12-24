7ee3b7936ff0a96c8743fb4d9b9c1a0b0d077f03
aleontiev <alonetiev@gmail.com>
2016-12-24 14:32:52 -0800

    ..

--- django_cli/blueprints/init/context.py
+++ django_cli/blueprints/init/context.py
@@ -5,11 +5,21 @@ import os
 
 @click.command()
 @click.argument('name')
-@click.option('--description', prompt='Description', default='N/A')
-@click.option('--author', prompt='Author name',
-              default=lambda: os.environ.get('USER', ''))
-@click.option('--email', prompt='Author email',
-              default=lambda: os.environ.get('USER', '') + '@me.com')
+@click.option(
+    '--description',
+    prompt='Description',
+    default='N/A'
+)
+@click.option(
+    '--author',
+    prompt='Author name',
+    default=lambda: os.environ.get('USER', '')
+)
+@click.option(
+    '--email',
+    prompt='Author email',
+    default=lambda: os.environ.get('USER', '') + '@me.com'
+)
 @click.option('--version', prompt='Version', default='0.0.1')
 @click.option('--django-version', prompt='Django version', default='1.10')
 def get_context(name, description, author, email, version, django_version):
